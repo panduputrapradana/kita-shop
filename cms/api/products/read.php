f51db54d91b554b764ec6fb5454fae6f53e9b217
panduputrapradana <panduandro17@gmail.com>
2026-01-29 09:58:15 +0700

    Lieur euyyy

--- cms/api/products/read.php
+++ cms/api/products/read.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 require_once "../../config/database.php";
 
--- cms/api/transaction/detail.php
+++ cms/api/transaction/detail.php
@@ -1,6 +1,10 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 require_once "../../config/database.php";
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
